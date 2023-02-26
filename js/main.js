21705de6752b27266060fb17edc04f54d14e5010
Nikolai Korolev <korolevns98@gmail.com>
2023-02-26 13:20:06 +0000

    Add status for saving

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function saveToDo() {
+    let status = document.getElementById("status")
+    status.innerText = "Saving..."
     let todo = document.getElementById("todo")
     document.cookie = "todo=" + todo.value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+    status.innerText = "Saved!"
 }
 
 function initialize() {

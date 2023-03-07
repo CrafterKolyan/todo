7d98a8d3ea7e5f8e4ca065ccf1834822c7b2bf0f
Nikolai Korolev <korolevns98@gmail.com>
2023-03-07 23:25:53 +0000

    Restrict 2 instances running at the same time

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Website: [https://crafterkolyan.github.io/todo/](https://crafterkolyan.github.io
     - Your ToDo is saved purely on your computer via [localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage) (preserved between browser sessions)
     - ToDo is saved on any change
     - `Ctrl + S` saves content (This feature is needed purely to keep users sane)
+- Detection of 2 instances running at the same time
+    - Check each second if another instance of application
+    - Automatically restricts edits to todo field and alerts user about another instance running
 
 ## Tested in browsers
 - Chrome
--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ function saveToDo() {
     status.innerText = "Saved!"
 }
 
+function checkSingleInstance() {
+    let currentInstanceId = parseInt(localStorage.getItem("currentInstanceId"))
+    if (currentInstanceId !== window.currentInstanceId) {
+        let todo = document.getElementById("todo")
+        todo.disabled = true
+
+        let status = document.getElementById("status")
+        status.innerText = "Another instance of this app is running"
+        status.className = "status right red"
+    }
+}
+
 function initialize() {
     document.addEventListener("keydown", (event) => {
         if (event.ctrlKey && event.key === "s") {
@@ -20,8 +32,18 @@ function initialize() {
     if (value !== null) {
         todo.value = decodeURIComponent(value)
     }
+    let currentInstanceId = localStorage.getItem("currentInstanceId")
+    if (currentInstanceId === null) {
+        currentInstanceId = 0
+    } else {
+        currentInstanceId = parseInt(currentInstanceId) + 1
+    }
+    localStorage.setItem("currentInstanceId", currentInstanceId)
+    window.currentInstanceId = currentInstanceId
     let status = document.getElementById("status")
     status.innerText = "Loaded!"
+
+    window.checkSingleInstanceInterval = setInterval(checkSingleInstance, 1000)
 }
 
 window.onload = initialize
